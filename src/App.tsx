98c1f718b6ea799247577d76fab9cca50ea8fc12
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-04-30 22:12:26 +0200

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <HomePage />
+      <Analytics />
       <Toaster />
     </>
   );
